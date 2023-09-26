0083794b1b5b715b52cfe893413b322178e3a667
Shuman kumari <95907851+Suman9496@users.noreply.github.com>
2023-09-26 21:53:01 +0530

    """The first line of code is a comment. The second line of code gets the first input number from the user. The float() function converts the input string to a floating-point number. The third line of code gets the second input number from the user. The float() function converts the input string to a floating-point number. The fourth line of code calculates the sum of the two numbers. The fifth line of code prints the sum to the console"""

---
+++ sum_of_2_num.py
@@ -0,0 +1,11 @@
+# Find the sum of two numbers
+
+# Get the input numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum of the two numbers
+sum = num1 + num2
+
+# Print the sum to the console
+print("The sum of the two numbers is", sum)
